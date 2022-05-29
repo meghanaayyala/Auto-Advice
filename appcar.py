2ce7f3b45af237cd68fb1a8215bce55148e2dcfe
Meghana Ayyala Somayajula <76214336+meghanaayyala@users.noreply.github.com>
2022-05-29 14:25:34 +0530

    Update appcar.py

--- appcar.py
+++ appcar.py
@@ -1136,7 +1136,13 @@ if nav=="Contact":
             <button type="submit">Send</button>
     </form>
     """
+    # Use Local CSS File
+    def local_css(file_name):
+        with open(file_name) as f:
+            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
+
+    local_css("style/style.css")
     st.markdown(contact_form, unsafe_allow_html=True)
 
     
